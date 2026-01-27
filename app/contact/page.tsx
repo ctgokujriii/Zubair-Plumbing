7b4a946af5a0ffe25ece91103eb9c93ef0736f11
unknown <ali.aleem75@gmail.com>
2026-01-27 17:42:37 +0500

    prototype 2

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -268,20 +268,7 @@ export default function Contact() {
             </div>
           </div>
 
-          <div className="bg-white rounded-2xl shadow-xl overflow-hidden">
-            <div className="h-96">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.2175771279363!2d-73.98823492346445!3d40.75889797138558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25855c6480299%3A0x55194ec5a1ae072e!2sTimes%20Square!5e0!3m2!1sen!2sus!4v1699999999999!5m2!1sen!2sus"
-                width="100%"
-                height="100%"
-                style={{ border: 0 }}
-                allowFullScreen
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-                title="Zubair Plumbing Location"
-              />
-            </div>
-          </div>
+         
         </div>
       </div>
 
